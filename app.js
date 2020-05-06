e1e9e6efdf8c3eaa6695294116458bf5b8e70500
b.s.x <b.s.x.mars@gmail.com>
2020-05-06 18:26:14 +0300

    added fs.appendFile

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ exports.fileContent = fs.readFile('text.txt', 'utf8', (err, data) => {
   }
   console.log(data);
 })
+
+exports.fileWrite = fs.appendFile('text.txt', "I was here\n", 'utf8', err => {
+  if(err) throw err
+})
