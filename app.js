62c4e01f9423ae032486c929f200f77ff8a5d071
b.s.x <b.s.x.mars@gmail.com>
2020-05-06 21:17:18 +0300

    rm node_mod

--- app.js
+++ app.js
@@ -9,4 +9,6 @@ exports.fileContent = fs.readFile('text.txt', 'utf8', (err, data) => {
 
 exports.fileWrite = fs.appendFile('text.txt', "I was here\n", 'utf8', err => {
   if(err) throw err
-})
+})   
+
+console.log();
