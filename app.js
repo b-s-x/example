93c750f580168edde6278a45c5b2df4b9127fdd7
b-s-x <b.s.x.mars@gmail.com>
2020-05-07 14:10:02 +0300

    added stream.pipe

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const zlib = require('zlib');
 
 module.exports.fileContent = function(cb) {
   fs.readFile('text.txt', 'utf8', cb)
@@ -7,3 +8,10 @@ module.exports.fileContent = function(cb) {
 exports.fileWrite = fs.appendFile('text.txt', "I was here\n", 'utf8', err => {
   if(err) throw err
 })
+
+const readStream = fs.createReadStream('text.txt', 'utf8');
+const writeStream = fs.createWriteStream('text.txt.gz');
+
+const gzip  = zlib.createGzip();
+
+readStream.pipe(gzip).pipe(writeStream);
