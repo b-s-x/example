98548158cffcb47111caaf689759ff5df7bfeaf7
b-s-x <b.s.x.mars@gmail.com>
2020-05-08 15:17:02 +0300

    added template

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const name = require('./user/person')
 const app = require('express')();
-const fsOps = require('./app')
-const Emitter = require('events')
+const Emitter = require('events');
+const fs = require('fs')
+const ops = require('./app')
 
 const port = 3000;
 const host = '127.0.0.1'
@@ -18,19 +19,31 @@ app.use((request, response, next) => {
   next()
 });
 
+app.set('view engine', 'pug');
+
+app.use('/home', (request, response) => {
+  response.render('home', {
+    title: "You're welcome",
+    main: 'Are u alive?'
+  })
+});
+
+app.use('/home', (request, response) => {
+  response.send('Main page');
+});
+
 app.get(`/${name.name}`, (request, response) => {
   response.send(`hello ${name.name}!`)
-  })
+});
 
 app.get('/', function(request, response) {
-  response.send('<h1>Home</h1>')
-  })
+  response.send('<h1>Home</h1>');
+});
 
 app.get('/about', function(request, response) {
   response.send('<h1>About</h1>')
-  })
-
+});
 
 app.listen(port, host, () => {
     console.log('Server is listening on 3000 port')
-  })
+});
